595eda9a995e585d380c3325297bafe260231d46
Brian McRae <mcrae.brian@gene.com>
2016-05-23 09:45:32 -0700

    Initial commit

---
+++ js/call.js
@@ -0,0 +1,36 @@
+var app = angular.module('acronymSearch', [])
+app.controller('DataController', function($scope) {
+	$scope.acronymList = "";
+	$scope.errorValue = false;
+	this.updateData = function init() {
+		$.ajax({
+  		url: [BACKEND ROUTE],
+		}).done(function(data) {
+			data = data.filter(function(obj) {
+				if (obj.name === null || obj.name === "") {
+					return false;
+				}
+				else {
+					return true;
+				}
+			});
+			$scope.acronymList = data;
+			$scope.$apply();
+		})
+		.fail(function() {
+			$scope.errorValue = true;
+			$scope.$apply();
+		});
+	};
+	$scope.startsWith = function(actual, expected) {
+		var lowerStr = (actual + "").toLowerCase();
+		return lowerStr.indexOf(expected.toLowerCase()) === 0;
+	};
+});
+app.directive('href', function() {
+  return {
+    compile: function(element) {
+      element.attr('target', '_blank');
+    }
+  };
+});
